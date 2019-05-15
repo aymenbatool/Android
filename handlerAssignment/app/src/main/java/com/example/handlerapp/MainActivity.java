df5199cc2f835648350498336c7afdd57710fd07
aymenbatool <aymenbatool21@gmail.com>
2019-05-15 07:05:30 +0500

    third commit

--- handlerAssignment/app/src/main/java/com/example/handlerapp/MainActivity.java
+++ handlerAssignment/app/src/main/java/com/example/handlerapp/MainActivity.java
@@ -1,7 +1,17 @@
 package com.example.handlerapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.annotation.SuppressLint;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.os.Handler;
+import android.os.Message;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,50 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button button = (Button) findViewById(R.id.myButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                handleButtonClick(v);
+            }
+        });
     }
+    private static final String MSG_KEY = "yo";
+
+    @SuppressLint("HandlerLeak")
+    private final Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            Bundle bundle = msg.getData();
+            String string = bundle.getString(MSG_KEY);
+            final TextView myTextView = (TextView)findViewById(R.id.myTextView);
+            myTextView.setText(string);
+        }
+    };
+
+    private final Runnable mMessageSender = new Runnable() {
+        public void run() {
+            Message msg = mHandler.obtainMessage();
+            Bundle bundle = new Bundle();
+            bundle.putString(MSG_KEY, getCurrentTime());
+            msg.setData(bundle);
+            mHandler.sendMessage(msg);
+        }
+    };
+
+    private String getCurrentTime() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss MM/dd/yyyy", Locale.US);
+        return dateFormat.format(new Date());
+    }
+
+    public void handleButtonClick(View view) {
+        new Thread(mMessageSender).start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //TODO not sure if this is needed for this use case
+        mHandler.removeCallbacks(mMessageSender);
+    }
+
 }
